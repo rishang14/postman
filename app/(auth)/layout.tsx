7af4410b54d5aa8d3a246a57ed6824be1076e532
Rishang <rishangkumar9693@gmail.com>
2025-10-02 21:36:54 +0530

    implemented better auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,18 @@
+import { auth } from "@/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import React from "react";
+
+const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    return redirect("/");
+  }
+
+  return <>{children}</>;
+};
+
+export default AuthLayout;
